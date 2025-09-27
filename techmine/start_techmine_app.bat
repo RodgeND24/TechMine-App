7194ebd2c0acedc2637333702fd36bcab617d101
RodgeND24 <dima.chelokov00@gmail.com>
2025-09-27 20:45:54 +0300

    script to open project in VScode

---
+++ techmine/start_techmine_app.bat
@@ -0,0 +1,9 @@
+@echo off
+
+@rem cd C:\Users\AndroidSDK\techmine_app\techmine
+@rem flutter pub get
+@rem flutter run --device-id chrome
+
+echo Start techmine_app project...
+code C:\Users\AndroidSDK\techmine_app\techmine
+pause
